b058ed13cbd137fc6a1eed0b72e798a2c16f5cae
kannavignesh <rickthetester@outlook.com>
2024-10-25 15:47:14 +1300

    Refact : changing localtestbase to testbase vignesh

--- src/main/java/Sprint_01/S6_57.java
+++ src/main/java/Sprint_01/S6_57.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class S6_57 extends LocalTestBase {
+public class S6_57 extends TestBase {
 	
 	@Test
 	public void testA() throws InterruptedException 
